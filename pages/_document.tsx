a9fb3f1e410e5d7a6f7c46f4e57166d2063be43e
elijahthis <emperorelijah007@gmail.com>
2023-03-05 00:01:31 +0100

    fixed btn issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head>
+				<title>Juju Swap</title>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
